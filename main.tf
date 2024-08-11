8f94c6a284a10a3a9f67af4d249b164627c200ba
Karthik Mirji <karthik.mirji@bloomreach.com>
2024-08-11 08:22:10 +0530

    resize num of instances

--- main.tf
+++ main.tf
@@ -51,9 +51,9 @@ resource "aws_eks_node_group" "terraform_nodegroup" {
     version = "$Latest"
   }
   scaling_config {
-    desired_size = 1
-    max_size     = 2
-    min_size     = 1
+    desired_size = 2
+    max_size     = 3
+    min_size     = 2
   }
 
 

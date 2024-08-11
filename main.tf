d4144f80fd2f59ef6fd76e1dea6b246958dc8e4b
Karthik Mirji <karthik.mirji@bloomreach.com>
2024-08-11 08:44:09 +0530

    reformat

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 provider "aws" {
   region = "us-east-1"
 }
+
 data "aws_caller_identity" "current" {}
 
 resource "aws_vpc" "my_vpc" {
@@ -10,6 +11,7 @@ resource "aws_vpc" "my_vpc" {
     Name = "my_vpc"
   }
 }
+
 # List of availability zones and corresponding CIDR blocks
 locals {
   availability_zones = ["us-east-1a", "us-east-1b", "us-east-1c"]
@@ -27,15 +29,18 @@ resource "aws_subnet" "my_subnets" {
     Name = "subnet-${each.key}"
   }
 }
+
 output "aws_account_id" {
   value = data.aws_caller_identity.current.account_id
 }
+
 resource "aws_eks_cluster" "terraform_cluster" {
   name     = "terraform-training-cluster"
   role_arn = aws_iam_role.terraform_cluster_role.arn
   vpc_config {
     subnet_ids = [for subnet in aws_subnet.my_subnets : subnet.id]
   }
+
   depends_on = [
     aws_iam_role.terraform_cluster_role
   ]
@@ -45,7 +50,7 @@ resource "aws_eks_node_group" "terraform_nodegroup" {
   cluster_name    = aws_eks_cluster.terraform_cluster.name
   node_group_name = "terraform-node-group"
   node_role_arn   = aws_iam_role.terraform_node_role.arn
-  subnet_ids = [for subnet in aws_subnet.my_subnets : subnet.id]
+  subnet_ids      = [for subnet in aws_subnet.my_subnets : subnet.id]
   launch_template {
     id      = aws_launch_template.eks_nodes.id
     version = "$Latest"
@@ -56,11 +61,10 @@ resource "aws_eks_node_group" "terraform_nodegroup" {
     min_size     = 2
   }
 
-
   depends_on = [
     aws_eks_cluster.terraform_cluster,
     aws_iam_role.terraform_node_role,
-    aws_iam_role_policy_attachment.terraform_cluster_role-AmazonEKSClusterPolicy
+    aws_iam_role_policy_attachment.terraform_cluster_role_AmazonEKSClusterPolicy
   ]
 }
 
@@ -71,8 +75,8 @@ resource "aws_iam_role" "terraform_cluster_role" {
     Version = "2012-10-17"
     Statement = [
       {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
+        Action    = "sts:AssumeRole"
+        Effect    = "Allow"
         Principal = {
           Service = "eks.amazonaws.com"
         }
@@ -88,8 +92,8 @@ resource "aws_iam_role" "terraform_node_role" {
     Version = "2012-10-17"
     Statement = [
       {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
+        Action    = "sts:AssumeRole"
+        Effect    = "Allow"
         Principal = {
           Service = "ec2.amazonaws.com"
         }
@@ -100,6 +104,7 @@ resource "aws_iam_role" "terraform_node_role" {
 
 resource "aws_launch_template" "eks_nodes" {
   instance_type = "t2.micro"
+
   tag_specifications {
     resource_type = "instance"
     tags = {
@@ -108,45 +113,38 @@ resource "aws_launch_template" "eks_nodes" {
   }
 }
 
-
-#resource "aws_iam_role_policy_attachment" "eks_role_attachment" {
-#  role       = aws_iam_role.terraform_cluster_role.name
-#  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-#}
-#
-#-----------------------------------------------------------------------------
-resource "aws_iam_role_policy_attachment" "terraform_cluster_role-AmazonEKSServicePolicy" {
+resource "aws_iam_role_policy_attachment" "terraform_cluster_role_AmazonEKSServicePolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
   role       = aws_iam_role.terraform_cluster_role.name
 }
 
-
-resource "aws_iam_role_policy_attachment" "terraform_cluster_role-AmazonEKS_CNI_Policy" {
+resource "aws_iam_role_policy_attachment" "terraform_cluster_role_AmazonEKS_CNI_Policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
   role       = aws_iam_role.terraform_node_role.name
 }
 
-resource "aws_iam_role_policy_attachment" "terraform_cluster_role-AmazonEC2ContainerRegistryReadOnly" {
+resource "aws_iam_role_policy_attachment" "terraform_cluster_role_AmazonEC2ContainerRegistryReadOnly" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
   role       = aws_iam_role.terraform_node_role.name
 }
 
-
-resource "aws_iam_role_policy_attachment" "terraform_cluster_role-AmazonEKSClusterPolicy" {
+resource "aws_iam_role_policy_attachment" "terraform_cluster_role_AmazonEKSClusterPolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
   role       = aws_iam_role.terraform_cluster_role.name
 }
 
-resource "aws_iam_role_policy_attachment" "terraform_cluster_role-AmazonEKSWorkerNodePolicy" {
+resource "aws_iam_role_policy_attachment" "terraform_cluster_role_AmazonEKSWorkerNodePolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-  role       = aws_iam_role.terraform_cluster_role.name
+  role       = aws_iam_role.terraform_node_role.name
 }
-
-
 
 resource "aws_eks_addon" "vpc_cni_addon" {
   cluster_name = aws_eks_cluster.terraform_cluster.name
   addon_name   = "vpc-cni"
+
+  depends_on = [
+    aws_eks_cluster.terraform_cluster
+  ]
 }
 
 resource "aws_eks_addon" "coredns_addon" {
@@ -154,10 +152,18 @@ resource "aws_eks_addon" "coredns_addon" {
   addon_name                  = "coredns"
   addon_version               = "v1.11.1-eksbuild.6"
   resolve_conflicts_on_update = "PRESERVE"
+
+  depends_on = [
+    aws_eks_cluster.terraform_cluster
+  ]
 }
 
 resource "aws_eks_addon" "kubeproxy_addon" {
-  cluster_name       = aws_eks_cluster.terraform_cluster.name
-  addon_name         = "kube-proxy"
+  cluster_name  = aws_eks_cluster.terraform_cluster.name
+  addon_name    = "kube-proxy"
   addon_version = "v1.30.0-eksbuild.3"
+
+  depends_on = [
+    aws_eks_cluster.terraform_cluster
+  ]
 }
